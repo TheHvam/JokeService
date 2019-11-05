b1cb38caf8fc39f8824f126a1cc1a92eec826e66
Martin <martin-yde-jp@hotmail.dk>
2019-11-05 09:46:06 +0100

    PRAY

---
+++ controllers/controller.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const Joke = require('../models/Jokes');
+
+exports.createJoke = function (setup, punchline) {
+    const  joke = new Joke({
+        setup: setup,
+        punchline: punchline
+    });
+    return joke.save();
+};
+
+exports.getJoke = function (jokeId) {
+    return Joke.findOne({_id: jokeId}).exec;
+};
+
+exports.getJokes = function () {
+    return Joke.find().populate().exec();
+}
---
+++ routes/joke.js
@@ -0,0 +1,19 @@
+const controller = require("../controllers/controller");
+const express = require('express');
+const router = express.Router();
+/*
+router
+        .get('/', (req, res) => {
+            controller.getJokes()
+                .then(val => res.json(val))
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).send(err);
+                });
+            })
+        .post('/', (req, res) => {
+            let joke = undefined;
+            const
+
+        })
+*/
