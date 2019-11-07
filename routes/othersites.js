49c20b318f7d9086ad4ee49ea2e4552493e8ed91
Martin <martin-yde-jp@hotmail.dk>
2019-11-07 11:09:47 +0100

    all but one works

--- routes/othersites.js
+++ routes/othersites.js
@@ -1,18 +1,19 @@
 const controller = require("../controllers/controller");
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch');
 
-function getOthersites (url) {
-    let arr = [];
-    fetch(url)
-        .then(response => response.json())
-        .then(array => arr = array);
-    return arr.exec;
-};
+async function GET(url) {
+    const OK = 200;
+    let res = await fetch(url);
+    if(res.status !== OK)
+        throw new Error(res.status);
+    return await res.json();
+}
 
 router
     .get('/', (req, res) => {
-        getOthersites('https://krdo-joke-registry.herokuapp.com/api/services')
+        GET('https://krdo-joke-registry.herokuapp.com/api/services')
             .then(val => res.json(val))
             .catch(err => {
                 console.log(err);
